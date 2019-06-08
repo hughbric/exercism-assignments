acbc967c372b412b09dc39a3f9a5033dec43411c
Michael Hughes <mhughes36@live.co.uk>
2019-06-09 00:08:03 +0100

    Comparisons

--- javascript/triangle/triangle.js
+++ javascript/triangle/triangle.js
@@ -1,6 +1,23 @@
-function Triangle() {
-  return 0;
+function Triangle(a, b, c) {
+  this.a = a;
+  this.b = b;
+  this.c = c;
+}
+
+Triangle.prototype.kind = function() {
+  if (this.a + this.b <= this.c || this.a + this.c <= this.b ||this.b + this.c <= this.a) {
+    throw new Error('Triangle violates triangle inequality');
+  }
+
+  if (this.a === this.b && this.a === this.c) {
+    return 'equilateral';
+  }
+
+  if (this.a === this.b || this.a === this.c || this.b === this.c) {
+    return 'isosceles';
+  } else {
+    return 'scalene';
+  }
 }
 
 export { Triangle };
-
