7ec4d526c0de094f62cee83e11c6f731ae37a392
Michael Hughes <mhughes36@live.co.uk>
2019-08-14 20:24:49 +0100

    string interpolation

---
+++ csharp/two-fer/TwoFer.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public static class TwoFer
+{
+    public static string Speak(string name = "")
+    {
+        return $"One for {(string.IsNullOrEmpty(name) ? "you" : name)}, one for me.";
+    }
+}
