a9549cb0244fbc1d6ecbdab4e717e5576ed230a2
Michael Hughes <mhughes36@live.co.uk>
2022-03-13 20:36:10 +0000

    Refactor solution to use gsub and more precise regexps

--- ruby/run-length-encoding/run_length_encoding.rb
+++ ruby/run-length-encoding/run_length_encoding.rb
@@ -1,39 +1,11 @@
 class RunLengthEncoding
-  def self.encode(input)
-    input_chars = input.split('')
-    encoded_chars = []
-    k = 0
-
-    while k < input_chars.length do
-      char_counter = 0
-
-      input_chars.each_with_index do |char, index|
-        if input_chars[k] == input_chars[index + k]
-          char_counter += 1
-        else
-          if char_counter > 1
-            encoded_chars << char_counter
-          end
-          encoded_chars << input_chars[k]
-          k += index
-          break
-        end
-      end
-    end
-    encoded_chars.join('')
+  def self.encode(plaintext)
+    regexp_capture_any_char_that_repeats_more_than_once = /(.)\1+/
+    plaintext.gsub(regexp_capture_any_char_that_repeats_more_than_once) { $&.length.to_s + $&.squeeze }
   end
 
-  def self.decode(input)
-    split_input = input.scan(/\d+|\D{1}/)
-    new_string = ''
-
-    split_input.each_with_index do |encode, index|
-      if /\d+/ === encode
-        new_string += (split_input[index + 1] * encode.to_i)
-      elsif !(/\d+/ === split_input[index - 1])
-        new_string += encode
-      end
-    end
-    new_string
+  def self.decode(compressed)
+    regexp_capture_repeating_digits_with_their_single_chars = /(\d+)(.)/
+    compressed.gsub(regexp_capture_repeating_digits_with_their_single_chars) { $2 * $1.to_i }
   end
 end
