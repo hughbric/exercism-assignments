1b2f5533ce68de8accf82081e555838b0ff086b3
Michael Hughes <mhughes36@live.co.uk>
2022-03-13 11:32:49 +0000

    Implement a Clock class with the following methods: to_s, +, -, ==

--- ruby/clock/clock.rb
+++ ruby/clock/clock.rb
@@ -1,21 +1,37 @@
 class Clock
+  attr_reader :hour, :minute
+
+  HOURS_PER_DAY = 24
+  MINUTES_PER_HOUR = 60
+  TIME_FORMAT = "%02d:%02d".freeze
+
   def initialize(hour: 0, minute: 0)
-    @daily_minutes = (hour * 60 + minute) % 1440
+    @hour = hour
+    @minute = minute
   end
 
   def to_s
-    format('%02d:%02d', hours, minutes)
+    compute_and_assign_hours_and_minutes
+    TIME_FORMAT % [@computed_hours, @computed_minutes]
+  end
+
+  def +(other_clock_instance)
+    Clock.new(hour: @hour + other_clock_instance.hour, minute: @minute + other_clock_instance.minute)
+  end
+
+  def -(other_clock_instance)
+    Clock.new(hour: @hour - other_clock_instance.hour, minute: @minute - other_clock_instance.minute)
+  end
+
+  def ==(other_clock_instance)
+    self.to_s == other_clock_instance.to_s
   end
 
   private
 
-  attr_reader :daily_minutes
-
-  def hours
-    daily_minutes / 60
-  end
-
-  def minutes
-    daily_minutes % 60
+  def compute_and_assign_hours_and_minutes
+    total_minutes = (@hour * MINUTES_PER_HOUR) + @minute
+    @computed_hours = (total_minutes / MINUTES_PER_HOUR) % HOURS_PER_DAY
+    @computed_minutes = total_minutes % MINUTES_PER_HOUR
   end
 end
