bb5e88d5e90ab30df5a2f2fefb5f08812332b42d
Michael Hughes <mhughes36@live.co.uk>
2022-03-26 20:48:49 +0000

    First solution to the transpose exercise

--- ruby/transpose/transpose.rb
+++ ruby/transpose/transpose.rb
@@ -1,7 +1,24 @@
-=begin
-Write your code for the 'Transpose' exercise in this file. Make the tests in
-`transpose_test.rb` pass.
+class Transpose
+  def self.transpose(input)
+    return input if input.empty?
 
-To get started with TDD, see the `README.md` file in your
-`ruby/transpose` directory.
-=end
+    lines = input.split("\n")
+    max_line_index = lines.index { |line| line == lines.max_by(&:length) }
+    result = []
+
+    lines.reverse_each { |line|
+      lines.index(line).downto(0) { |descending_index| lines[descending_index] = lines[descending_index].ljust(line.length) }
+    }
+    row_length = lines.first.length || 0
+
+    row_length.times { |index|
+      row = []
+      lines.each { |line|
+        row << line[index]
+      }
+      row << "\n"
+      result << row
+    }
+    result.join('').chop
+  end
+end
