3fd9571271884fe22f2f2034c5c4a6b91a5aa375
Michael Hughes <mhughes36@live.co.uk>
2022-03-26 21:20:28 +0000

    Refactor variable names

--- ruby/transpose/transpose.rb
+++ ruby/transpose/transpose.rb
@@ -2,10 +2,10 @@ class Transpose
   def self.transpose(input)
     return input if input.empty?
 
-    char_lines = input.gsub(' ', '_').split("\n")
-    max_length = char_lines.max_by(&:length).length
+    lines = input.gsub(' ', '_').split("\n")
+    max_length = lines.max_by(&:length).length
 
-    char_lines.map { |char_line| char_line.ljust(max_length).chars }
+    lines.map { |line| line.ljust(max_length).chars }
       .transpose
       .map { |char_line| char_line.join.rstrip.gsub('_', ' ') }
       .join("\n")
