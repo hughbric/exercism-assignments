0588ccc7864750655fd7f1d761af20c4abeccbe6
Michael Hughes <mhughes36@live.co.uk>
2022-03-26 13:02:09 +0000

    Refactor to use the step function

--- ruby/sum-of-multiples/sum_of_multiples.rb
+++ ruby/sum-of-multiples/sum_of_multiples.rb
@@ -1,16 +1,11 @@
 class SumOfMultiples
   def initialize(*multiples)
-    @multiples = multiples.filter { |m| m != 0 }
+    @multiples = multiples.filter{ |m| m != 0 }
   end
 
   def to(final_number)
-    return 0 if @multiples.empty?
-    (0...final_number).select{ |number| multiple?(number) }.reduce(:+)
-  end
-
-  private
-
-  def multiple?(number)
-    @multiples.any? { |multiple| number % multiple == 0 }
+    @multiples.flat_map do |multiple|
+      (multiple...final_number).step(multiple).to_a
+    end.uniq.sum
   end
 end
